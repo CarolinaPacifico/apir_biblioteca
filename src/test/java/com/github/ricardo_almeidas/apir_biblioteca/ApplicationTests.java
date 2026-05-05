d5c16be2d93def01ef90616f69acd360483a9bf5
CarolinaPacifico <carolina.b.pacifico@gmail.com>
2026-05-04 21:15:26 -0300

    add: cp2

--- src/test/java/com/github/ricardo_almeidas/apir_biblioteca/ApplicationTests.java
+++ src/test/java/com/github/ricardo_almeidas/apir_biblioteca/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.ricardo_almeidas.apir_biblioteca;
+package com.github.carolinapacifico.apir_biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
